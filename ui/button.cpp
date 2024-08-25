4bcaf022cb63788bac1e2e74f6df62f19527d390
objectDisorientedProgrammer <tikiman92@gmail.com>
2024-08-25 16:34:52 -0700

    updated button class

--- ui/button.cpp
+++ ui/button.cpp
@@ -1,62 +1,92 @@
 #include "button.h"
 
-#define TESTING 0
-
-Button::Button(int x, int y, std::string text=std::string(""), int width=60, int height=30, bool enable=true) : bx(x), by(y), btext(text), bw(width), bh(height), benabled(enable)
+namespace wtc
 {
-    // determine button text length
-    bfontsize = 11;
-    if (!btext.empty())
+    namespace ui
     {
-        btextlength = MeasureText(btext.c_str(), bfontsize);
-    }
-    else
-        btextlength = 0;
-
-    state = btnstate::none;
-    bcolor = CLITERAL(Color){ 102, 191, 255, 200 };
-    bautomatic_resize = false;
-}
-
-#if TESTING
-
-#include <vector>
-
-int main()
-{
-    std::string window_title{"UI testing"};
-    InitWindow(1000, 650, window_title.c_str());
-
-    constexpr int number_of_buttons{6};
-    std::vector<Button> btns;
-
-    for (int i = 0; i < number_of_buttons; ++i)
-    {
-        btns.push_back(Button(100+70*i, 100+40*i, std::to_string(i+1)));
-    }
-
-    MouseInfo m;
-
-    SetTargetFPS(60);
-    // main game loop
-    while (!WindowShouldClose())
-    {
-        m.update();
-
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-        for (std::vector<Button>::iterator i = btns.begin(); i != btns.end(); ++i)
+        Button::Button(int x, int y, std::string text, int width, int height, bool enable) : bx(x), by(y), btext(text), bw(width), bh(height), benabled(enable)
         {
-            i->update(m);
-            i->draw();
+            // determine button text length
+            bfontsize = 11;
+            if (!btext.empty())
+            {
+                btextlength = MeasureText(btext.c_str(), bfontsize);
+            }
+            else
+                btextlength = 0;
+
+            state = btnstate::none;
+            bcolor = CLITERAL(Color){ 102, 161, 205, 255 };
+            bautomatic_resize = false;
         }
 
-        m.draw();
-        EndDrawing();
-    }
+        btnstate Button::update(MouseInput& mouse)
+        {
+            unsigned char alpha;
+            // mouse within button bounds
+            if (is_selected(mouse.get_x(), mouse.get_y()))
+            {
+                state = btnstate::hover;
+                alpha = 255;//200; // TODO change alpha reductions to RGB reductions
+                bcolor.r = 102;//52;
+                bcolor.g = 161;//141;
+                bcolor.b = 225;
+                if (mouse.is_left_pressed())
+                {
+                    state = btnstate::press;
+                    alpha = 255;//150;
+                    bcolor.r = 102;//2;
+                    bcolor.g = 161;//91;
+                    bcolor.b = 255;//155;
+                }
+            }
+            else
+            {
+                state = btnstate::idle;
+                alpha = 255; // default for btnstate::idle/none
+                bcolor.r = 102;//102, 191, 255
+                bcolor.g = 161;
+                bcolor.b = 205;
+            }
 
-    CloseWindow();
-    return 0;
-}
-#endif
+            bcolor.a = alpha;
+
+            return state;
+        }
+
+        void Button::draw()
+        {
+            if (bautomatic_resize)
+            {
+                // resize button to fit text
+                if (btextlength > bw)
+                {
+                    bw = btextlength + 5;
+                }
+                if (btextlength > bh)
+                {
+                    bh = btextlength + 5;
+                }
+            }
+
+            // TODO color change for enabled/disabled
+
+            DrawRectangleRounded({static_cast<float>(bx), static_cast<float>(by), static_cast<float>(bw), static_cast<float>(bh)}, 0.6f, 10, bcolor);
+            if (!btext.empty())
+                DrawText(btext.c_str(), bx + bw / 2 - btextlength / 2, by + bh / 3, bfontsize, BLACK);
+        }
+
+        bool Button::is_selected(int x, int y)
+        {
+            return (bx < x && x < bx + bw && by < y && y < by + bh);
+        }
+
+        void Button::set_color(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha)
+        {
+            bcolor.r = red;
+            bcolor.g = green;
+            bcolor.b = blue;
+            bcolor.a = alpha;
+        }
+    } // ui
+} // wtc
--- ui/button.h
+++ ui/button.h
@@ -1,142 +1,55 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include "mouse_input.h"
 #include "raylib.h"
 #include <string>
 
-class MouseInfo
+namespace wtc
 {
-public:
-    // MouseInfo();
-    // ~MouseInfo();
-
-    int x;
-    int y;
-    bool pressed_left;
-    bool pressed_right;
-    bool pressed_middle;
-
-    //const MouseInfo& get() { return this; }
-    void update()
+    namespace ui
     {
-        x=GetMouseX();
-        y=GetMouseY();
-        pressed_left=IsMouseButtonPressed(MOUSE_BUTTON_LEFT)||IsMouseButtonDown(MOUSE_BUTTON_LEFT);
-        pressed_right=IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)||IsMouseButtonDown(MOUSE_BUTTON_RIGHT);
-        pressed_middle = IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)||IsMouseButtonDown(MOUSE_BUTTON_MIDDLE);
-    }
 
-    void draw()
-    {
-        // debugging info
-        std::string dbg{std::string("x:")+std::to_string(x)+std::string(" y:")+std::to_string(y)+std::string(" ")};
-        if (pressed_left)
+        enum class btnstate
         {
-            dbg += std::string("LEFT");
-        }
-        else if (pressed_right)
+            none, idle, hover, press
+        };
+
+        class Button
         {
-            dbg += std::string("RGHT");
-        }
-        else if (pressed_middle)
-        {
-            dbg += std::string("MIDL");
-        }
-        DrawText(dbg.c_str(), 5, 630, 16, BLACK);
-    }
-    
-};
+        public:
+            Button() = default;
+            Button(int x, int y, std::string text=std::string(""), int width=60, int height=30, bool enable=true);
+            ~Button() {}
 
+            btnstate update(MouseInput& mouse);
 
+            void draw();
 
-enum class btnstate
-{
-    none, idle, hover, press
-};
+            bool is_selected(int x, int y);
+            void set_enabled(bool enable) { benabled = enable; }
+            bool is_enabled() { return benabled; }
 
-class Button
-{
-public:
-    Button() = default;
-    Button(int x, int y, std::string text, int width, int height, bool enable);
-    ~Button() {}
-    
-    btnstate update(MouseInfo& mouse) // TODO generalize as 'inputinfo' or similar...
-    {
-        unsigned char alpha;
-        // mouse within button bounds
-        if (bx < mouse.x && mouse.x < bx + bw && by < mouse.y && mouse.y < by + bh)
-        {
-            state = btnstate::hover;
-            alpha = 150; // TODO change alpha reductions to RGB reductions
-            if (mouse.pressed_left)
-            {
-                state = btnstate::press;
-                alpha = 100;
-            }
-        }
-        else
-        {
-            state = btnstate::idle;
-            alpha = 200; // default for btnstate::idle/none
-        }
+            int get_x() { return bx; }
+            void set_x(int x) { bx = x; }
+            int get_width() { return bw; }
+            int get_height() { return bh; }
+            void set_height(int h) { bh = h; }
 
-        bcolor.a = alpha;
+            void set_color(CLITERAL(Color) color) { bcolor = color; }
+            void set_color(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha);
 
-        return state;
-    }
-    void draw()
-    {
-        if (bautomatic_resize)
-        {
-            // resize button to fit text
-            if (btextlength > bw)
-            {
-                bw = btextlength + 5;
-            }
-            if (btextlength > bh)
-            {
-                bh = btextlength + 5;
-            }
-        }
-
-        // TODO color change for enabled/disabled
-
-        DrawRectangleRounded({static_cast<float>(bx), static_cast<float>(by), static_cast<float>(bw), static_cast<float>(bh)}, 0.6f, 10, bcolor);
-        if (!btext.empty())
-            DrawText(btext.c_str(), bx + bw / 2 - btextlength / 2, by + bh / 3, bfontsize, BLACK);
-    }
-    bool is_pressed(int x, int y)
-    {
-        return (bx < x && x < bx + bw && by < y && y < by + bh);
-    }
-    void set_enabled(bool enable) { benabled = enable; }
-    bool is_enabled() { return benabled; }
-
-    int get_x() { return bx; }
-    void set_x(int x) { bx = x; }
-    int get_width() { return bw; }
-    int get_height() { return bh; }
-    void set_height(int h) { bh = h; }
-
-    void set_color(CLITERAL(Color) color) { bcolor = color; }
-    void set_color(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha)
-    {
-        bcolor.r = red;
-        bcolor.g = green;
-        bcolor.b = blue;
-        bcolor.a = alpha;
-    }
-
-private:
-    bool benabled;
-    int bx, by, bw, bh;
-    bool bautomatic_resize; // resize button dimensions to fit text
-    std::string btext;
-    int bfontsize; // font size (e.g. 10pt, 14pt, 26pt)
-    int btextlength; // length of button text in pixels
-    btnstate state;
-    CLITERAL(Color) bcolor;
-};
+        private:
+            bool benabled;
+            int bx, by, bw, bh;
+            bool bautomatic_resize; // resize button dimensions to fit text
+            std::string btext;
+            int bfontsize; // font size (e.g. 10pt, 14pt, 26pt)
+            int btextlength; // length of button text in pixels
+            btnstate state;
+            CLITERAL(Color) bcolor;
+        };
+    } // ui
+} // wtc
 
 #endif
